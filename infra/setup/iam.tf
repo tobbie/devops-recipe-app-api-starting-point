b97c36480088ecc66c7d0af5afda94e552e7f49a
Oluwatobi Akinseye <tobi.akinseye@gmail.com>
2025-01-02 15:26:18 +0100

    correct typo in the assignment of TF_VAR_db_password env variable-2

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -174,7 +174,9 @@ data "aws_iam_policy_document" "rds" {
       "rds:CreateDBInstance",
       "rds:DeleteDBInstance",
       "rds:ListTagsForResource",
-      "rds:ModifyDBInstance"
+      "rds:ModifyDBInstance",
+      "rds:AddTagsToResource",
+      "rds:ModifyDBSubnetGroup"
     ]
     resources = ["*"]
   }
